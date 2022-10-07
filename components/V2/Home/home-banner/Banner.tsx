e48a4c1e75dbe9528e8a61ab7b466a64f7ace4f4
its-kayes <kayes.ek8@gmail.com>
2022-10-08 02:04:11 +0600

    basic stracture of banner is done

--- components/V2/Home/home-banner/Banner.tsx
+++ components/V2/Home/home-banner/Banner.tsx
@@ -2,6 +2,9 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import { Button } from "@mui/material";
+import style from "../../../../styles/V2/Home/HomeBanner.module.css";
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -19,13 +22,43 @@ export default function Banner() {
       marginTop={3}
       sx={{ flexGrow: 1 }}
     >
-      <Grid container columns={16}>
+      <Grid
+        container
+        paddingTop={15}
+        paddingX={12}
+        columns={16}
+        className="flex items-center"
+      >
         <Grid item xs={8}>
-          <Item>xs=8</Item>
+          <Box>
+            <h1 className={style.header1}> Hello! </h1>
+            <h1 className={style.header2}> This is Emrul Kayes </h1>
+            <Box
+              paddingLeft={1.2}
+              color="#8892B0"
+              marginTop={3}
+              marginBottom={5}
+            >
+              I am a web developer. In the last few months I have completed more
+              than 10+ websites project. Im expert in HTML, CSS and JavaScript.
+              As a library component Im expert in both Bootstrap and
+              TailwindCSS. Also expert in React js as a JavaScript framework. As
+              a backend good at Node.js and Express.js. Also have good knowledge
+              at Mongodb database
+            </Box>
+            <a
+              href="resume.pdf"
+              className=" flex items-center w-1/4 justify-evenly ml-3 py-3 rounded-lg border-2 text-[#4AB9A7] font-semibold border-sky-300 normal-case hover:text-black hover:bg-sky-100"
+              download
+            >
+              {" "}
+              <i>Download Resume</i> <CloudDownloadIcon />
+            </a>
+          </Box>
         </Grid>
         <Grid display="flex" justifyContent="center" item xs={8}>
           <img
-            className="w-1/3 rounded-3xl max-w-lg h-auto rounded-lg transition-all duration-600 cursor-pointer filter grayscale hover:grayscale-0"
+            className="w-2/4 rounded-3xl max-w-lg rounded-lg transition-all duration-600 cursor-pointer filter hover:grayscale grayscale-0"
             src="/profile.jpg"
             alt="image description"
           />
