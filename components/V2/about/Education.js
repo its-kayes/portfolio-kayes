0902780dd1b99b9236bda8753913d6b52e98dbb1
its-kayes <kayes.ek8@gmail.com>
2022-11-01 00:15:02 +0600

    Setup about page

---
+++ components/V2/about/Education.js
@@ -0,0 +1,24 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+
+const Education = () => {
+  return (
+    <div>
+      <div>
+        <p className="font-bold text-xl text-white">
+          B.Sc. in Computer Science and Engineering
+        </p>
+        <p className="text-white py-1">
+          Shanto-Mariam University of Creative Technology
+        </p>
+        <p className="my-3">
+          Currently I'm a student of Last Year. Despite having programming
+          skills from my previous study, I find Web Development more
+          interesting.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Education;
---
+++ components/V2/about/Experience.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div>
+      <div>
+        <p className="font-bold text-2xl text-[#e4e7ea]">Team Based Project</p>
+        <p className="pb-4 pt-2 font-mono font-semibold text-[#b6c2eb]">
+          Being a team leader I do guide my team members and complete few
+          projects
+        </p>
+        <i className="">
+          Although I dont have any professional work experience but Being a team
+          leader I do guide my team members and complete few projects.
+        </i>
+        <span className="text-blue-600 hover: hover:font-semibold">
+          Check...
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
